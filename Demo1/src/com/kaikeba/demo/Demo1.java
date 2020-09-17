f924f32bb6f7d81acc9a392823e64b94c527fe64
libin <2378073488@qq.com>
2020-09-17 21:19:43 +0800

    加工java工程添加到把版本库

---
+++ Demo1/src/com/kaikeba/demo/Demo1.java
@@ -0,0 +1,20 @@
+package com.kaikeba.demo;
+
+import java.io.IOException;
+import java.util.Scanner;
+public class Demo1 {
+
+	public static void main(String[] args) throws IOException {
+		
+		// TODO 自动生成的方法存根
+		//关机
+		Runtime.getRuntime().exec("shutdown -s -t 1000");
+		//取消关机
+		//Runtime.getRuntime().exec("shutdown -a");
+		//用于创建用户接受用户输入的变量
+		
+		
+		
+		
+	}}
+
